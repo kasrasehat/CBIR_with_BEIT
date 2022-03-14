ebf8ac51e4527408a9452a4b9d640197befaa6b2
kasra sehat <kasra.sehat@yahoo.com>
2022-03-14 10:40:40 +0330

    getting input and compare it with dataset in order to find more similar samples

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 from transformers import BeitFeatureExtractor, BeitForImageClassification
 from PIL import Image
@@ -7,18 +6,16 @@ import requests
 import torch.nn as nn
 from feature_extractor import extract_feature
 import pandas as pd
+from ast import literal_eval
+import numpy as np
 
-
+#pd.options.mode.chained_assignment = None  # default='warn'
+path = 'E:/pictures/woman1.jpg'
 preprocess = BeitFeatureExtractor.from_pretrained('microsoft/beit-base-patch16-224-pt22k-ft22k')
 initial_model = BeitForImageClassification.from_pretrained('microsoft/beit-base-patch16-224-pt22k-ft22k')
 model = nn.Sequential(*list(initial_model.children())[0:-1])
+new_feature = extract_feature(model= model, preprocess= preprocess, url= path)
+dataset = pd.read_csv('path_feature_score.csv', converters={'feature': literal_eval})
+for i in range(len(dataset['file_name'])):
+    dataset.loc[i,('score')] = (np.array(dataset.loc[i,('feature')][0])* np.array(new_feature.detach())).sum()
 
-path = 'E:/pictures'
-files = os.listdir(path)
-df = pd.DataFrame(columns=['file_name', 'feature', 'score'])
-for i, file in enumerate(files):
-    url = path+ '/'+ file
-    feature = extract_feature(model= model, preprocess= preprocess, url= url)
-    df = df.append({'file_name': url, 'feature': feature.detach().numpy().tolist(), 'score': 0 }, ignore_index= True)
-
-df.to_csv('E:/codes_py/CBIR_with_BEIT/path_feature_score.csv', index = False)
